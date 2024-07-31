f385a848e26ff019d3f219ed8c678ee27bd43410
Hasan Yahya <hasna786yahya@gmail.com>
2024-08-01 01:07:00 +0500

    chore: Fix comment indentation in hamburger-menu component

--- components/hamburger-menu.tsx
+++ components/hamburger-menu.tsx
@@ -3,11 +3,11 @@ import React, { useState } from 'react';
 
 const HamburgerMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
-    
+
     return (
         <View
             className='flex-row items-center gap-2'
-            //onClick={() => setIsOpen(!isOpen)}
+        //onClick={() => setIsOpen(!isOpen)}
         >
             <View className='w-6 h-1 bg-foreground' />
             <View className='w-6 h-1 bg-foreground' />
