c61ad82abbe9d9484fc2be3d81f181d7cb07ab4e
Hasan Yahya <hasna786yahya@gmail.com>
2024-08-01 03:50:57 +0500

    feat: Add lazy loading for images in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -16,25 +16,40 @@ import { Progress } from '~/components/ui/progress';
 import { Text } from '~/components/ui/text';
 import { Tooltip, TooltipContent, TooltipTrigger } from '~/components/ui/tooltip';
 import { Plus } from '~/lib/icons/Plus';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, ScrollView } from 'react-native';
 
 export default function Screen() {
 
+  const days = [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()];
+  const formatDate = (date: Date) => {
+    return date.toLocaleDateString('en-US', {
+      weekday: 'short', month: 'short', day: 'numeric'
+    });
+  };
+
   return (
-    <View className='z-10 flex-1 justify-center items-center gap-5 p-6 bg-secondary/30'>
+    <View className='z-10 flex-1 justify-center items-center gap-0 p-0 bg-secondary/30'>
       <TouchableOpacity
         className='z-30 absolute bottom-8 right-8 rounded-full bg-blue-500 h-[68px] w-[68px] flex items-center justify-center'
       >
         <Plus className='text-white' size={26}
         />
       </TouchableOpacity>
-      <View className='flex-1 w-full'>
-        {/*Dates*/}
-        <View className='flex flex-row justify-between items-center'>
-          <Text className='text-primary text-lg font-bold'>Today</Text>
-          <Text className='text-primary text-lg font-bold'>Tomorrow</Text>
-        </View>
-      </View>
+      <ScrollView
+        horizontal
+        contentContainerStyle={{ paddingHorizontal: 10 }}
+        showsHorizontalScrollIndicator={false}
+        className='w-full gap-6 mt-6'
+      >
+        {days.map((day, index) => (
+          <TouchableOpacity
+            key={index}
+            className='p-3 bg-gray-200 rounded-full shadow-md mx-1.5 h-12'
+          >
+            <Text className='text-primary font-bold text-white dark:text-black'>{formatDate(day)}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 }
