c45debcae5b820789862edbb0dffd1166f08e872
Hasan Yahya <hasna786yahya@gmail.com>
2024-08-02 07:07:26 +0500

    refactor: Update active tab styling and functionality in index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -28,35 +28,67 @@ export default function Screen() {
 
 	return (
 		<View className='z-10 flex-1 justify-start gap-0 p-0 bg-white dark:bg-black h-full'>
-			{/*Tabs*/}
+			{/* Tabs */}
 			<View className="flex-row border-b border-gray-400 mb-4 mx-0 mt-[22px]">
 				<TouchableOpacity
-					className={`ml-6 mr-4 flex-row items-center pb-2 ${activeTab === 'Starred' ? 'border-b-2 border-blue-500' : ''}`}
+					className="ml-6 mr-4 flex-row items-center pb-2 relative"
 					onPress={() => setActiveTab('Starred')}
 				>
 					<Star className={`w-4 h-4 ${activeTab === 'Starred' ? 'text-blue-500' : 'text-black'}`} />
+					{activeTab === 'Starred' && (
+						<View style={{
+							position: 'absolute',
+							bottom: -1,
+							left: 0,
+							right: 0,
+							height: 4,
+							borderTopLeftRadius: 4,
+							borderTopRightRadius: 4,
+						}} className='bg-blue-500' />
+					)}
 				</TouchableOpacity>
 				<TouchableOpacity
-					className={`ml-4 mr-4 flex-row items-center pb-2 ${activeTab === 'My Tasks' ? 'border-b-2 border-blue-500' : ''}`}
+					className="ml-4 mr-4 flex-row items-center pb-2 relative"
 					onPress={() => setActiveTab('My Tasks')}
 				>
 					<Text className={`text-[16px] ml-1 mr-1 ${activeTab === 'My Tasks' ? 'text-blue-500' : 'text-black'}`}>My Tasks</Text>
+					{activeTab === 'My Tasks' && (
+						<View style={{
+							position: 'absolute',
+							bottom: -1,
+							left: 0,
+							right: 0,
+							height: 4,
+							borderTopLeftRadius: 4,
+							borderTopRightRadius: 4,
+						}} className='bg-blue-500' />
+					)}
 				</TouchableOpacity>
 				<TouchableOpacity
-					className={`ml-4 mr-6 flex-row items-center pb-2 ${activeTab === 'New list' ? 'border-b-2 border-blue-500' : ''}`}
+					className="ml-4 mr-6 flex-row items-center pb-2 relative"
 					onPress={() => setActiveTab('New list')}
 				>
 					<Plus className={`w-4 h-4 mr-2 ${activeTab === 'New list' ? 'text-blue-500' : 'text-black'}`} />
 					<Text className={`text-[16px] mr-2 ${activeTab === 'New list' ? 'text-blue-500' : 'text-black'}`}>New list</Text>
+					{activeTab === 'New list' && (
+						<View style={{
+							position: 'absolute',
+							bottom: -1,
+							left: 0,
+							right: 0,
+							height: 4,
+							borderTopLeftRadius: 4,
+							borderTopRightRadius: 4,
+						}} className='bg-blue-500' />
+					)}
 				</TouchableOpacity>
 			</View>
 
-			{/*Hovering Icon*/}
+			{/* Hovering Icon */}
 			<TouchableOpacity
 				className='z-30 absolute bottom-8 right-8 rounded-full bg-blue-400 dark:bg-white h-[68px] w-[68px] flex items-center justify-center'
 			>
-				<Plus className='text-white dark:text-black' size={26}
-				/>
+				<Plus className='text-white dark:text-black' size={26} />
 			</TouchableOpacity>
 		</View>
 	);
